de07185b553127ce9c24cb25da5989a1035ce227
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-03 19:39:53 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,7 @@ end
 
 def literal_aoh
   assembled_aoh = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
-  assembled_aoh = Array.new.sort
+  assembled_aoh.sort = Array.new
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
