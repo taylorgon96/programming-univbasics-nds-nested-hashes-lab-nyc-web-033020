c18e60e053c7fb044a0bcdb9d47c903b1b946894
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-03 20:04:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,14 @@ def assembled_aoh
 end
 
 def literal_aoh
-  literal_aoh = Array.new.sort
+  literal_aoh = 
+  { name:  "Don Gately", occupation: "Live-in Staff" }
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+  { name:  "Pat Monteseian", occupation: "Staff" }
+  { name:  "Kate Gompert", occupation: "None" }
+  { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  
+  literal_aoh = Array.new
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
