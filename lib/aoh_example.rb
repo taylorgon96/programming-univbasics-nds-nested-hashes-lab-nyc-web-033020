2f80e973f90688a789c5e27feb38442a58c4db11
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-03 19:42:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,7 @@ def literal_aoh
 { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 { name:  "Pat Monteseian", occupation: "Staff" }
 { name:  "Kate Gompert", occupation: "None" }
-{ name:  "Bruce Green", occupation: "Fan of Mildred" } = Array.new
+{ name:  "Bruce Green", occupation: "Fan of Mildred" }= Array.new
 
  
   # Using Array literal syntax only, build a nested array that uses the data in
